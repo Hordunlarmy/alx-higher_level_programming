e6455b4dbe998cd9cc2a22937577f56a6c929ebf
Hordunlarmy <Hordunlarmy@gmail.com>
2023-08-29 11:08:39 +0100

    Refactor safe_print_list_integers function to improve readability

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-      # A function that prints the first x elements
-      # of a list and only integers.
+    # A function that prints the first x elements of a list and only integers.
 
-      index = 0
-      for i in range(x):
-            try:
-                  print("{:d}".format(my_list[i]), end="")
-                  index += 1
-
-            except (ValueError, TypeError):
-                  continue
-
-      print()
-      return (index)
+    index = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            index += 1
+        except (TypeError, ValueError):
+            continue
+    print()
+    return (index)
