0542092d4ec2225236ebf5752595df6f5f69d247
Hordunlarmy <Hordunlarmy@gmail.com>
2023-08-29 22:42:59 +0100

    Refactor size setter in Square class to raise a TypeError if value is not an integer and a ValueError if value is less than 0

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -19,14 +19,15 @@ class Square:
 
     @property
     def size(self):
-        return self.__size
+        return (self.__size)
 
     @size.setter
     def size(self, value):
-        if type(value) is not int:
-            raise TypeError("size must be an integer")
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
         if (value < 0):
-            raise ValueError("size must be >= 0")
+            raise ValueError('size must be >= 0')
+        self.__size = value
 
     def area(self):
         return (self.__size ** 2)
