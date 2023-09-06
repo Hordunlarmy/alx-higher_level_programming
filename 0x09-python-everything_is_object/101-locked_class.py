54336db85a763d976f30fa709374fae4bc90cc57
Hordunlarmy <Hordunlarmy@gmail.com>
2023-09-06 16:49:24 +0100

    Add locked class with limited instance attributes

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""locked class that prevents the user from dynamically creating new instance attributes
+"""
+
+
+class LockedClass:
+    """allow only first_name attribute as new instance"""
+    __slots__ = ('first_name')
+    pass
