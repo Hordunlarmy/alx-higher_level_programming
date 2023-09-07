005f2a43995e5e0a8f5bdab52f35eca24e0029e2
Hordunlarmy <Hordunlarmy@gmail.com>
2023-09-07 01:15:02 +0100

    Fix bug in xout function

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,71 +1,103 @@
 #!/usr/bin/python3
-"""N-queens puzzle."""
+"""N-queens puzzle"""
 
 import sys
 
 
 def init_board(n):
-    """Initialize an `n`x`n` sized chessboard with 0's."""
-    board = [[' ' for _ in range(n)] for _ in range(n)]
-    return board
+    """Initialize an n x n sized chessboard with 0's."""
+    board = []
+    [board.append([]) for i in range(n)]
+    [row.append(" ") for i in range(n) for row in board]
+    return (board)
 
 
 def board_deepcopy(board):
-    """Return a deepcopy of a chessboard."""
+    """Returns a deepcopy of a chessboard."""
     if isinstance(board, list):
-        return [board_deepcopy(row) for row in board]
-    return board
+        return (list(map(board_deepcopy, board)))
+    return (board)
 
 
 def get_solution(board):
-    """Return the list of lists representation of a solved chessboard."""
+    """Returns the list of lists representation of a solved chessboard."""
     solution = []
     for r in range(len(board)):
         for c in range(len(board)):
             if board[r][c] == "Q":
                 solution.append([r, c])
                 break
-    return solution
+    return (solution)
 
 
 def xout(board, row, col):
     """X out spots on a chessboard.
-    All spots where non-attacking queens can no
-    longer be played are X-ed out.
     """
-    directions = [(0, 1), (0, -1), (1, 0), (-1, 0),
-                  (1, 1), (-1, -1), (-1, 1), (1, -1)]
-
-    for dr, dc in directions:
-        r, c = row + dr, col + dc
-        while 0 <= r < len(board) and 0 <= c < len(board):
-            board[r][c] = "x"
-            r, c = r + dr, c + dc
+    # X out all forward spots
+    for c in range(col + 1, len(board)):
+        board[row][c] = "x"
+    # X out all backwards spots
+    for c in range(col - 1, -1, -1):
+        board[row][c] = "x"
+    # X out all spots below
+    for r in range(row + 1, len(board)):
+        board[r][col] = "x"
+    # X out all spots above
+    for r in range(row - 1, -1, -1):
+        board[r][col] = "x"
+    # X out all spots diagonally down to the right
+    c = col + 1
+    for r in range(row + 1, len(board)):
+        if c >= len(board):
+            break
+        board[r][c] = "x"
+        c += 1
+    # X out all spots diagonally up to the left
+    c = col - 1
+    for r in range(row - 1, -1, -1):
+        if c < 0:
+            break
+        board[r][c]
+        c -= 1
+    # X out all spots diagonally up to the right
+    c = col + 1
+    for r in range(row - 1, -1, -1):
+        if c >= len(board):
+            break
+        board[r][c] = "x"
+        c += 1
+    # X out all spots diagonally down to the left
+    c = col - 1
+    for r in range(row + 1, len(board)):
+        if c < 0:
+            break
+        board[r][c] = "x"
+        c -= 1
 
 
 def recursive_solve(board, row, queens, solutions):
-    """Recursively solve an N-queens puzzle.
+    """Recursively solves an N-queens puzzle.
     """
     if queens == len(board):
         solutions.append(get_solution(board))
-        return solutions
+        return (solutions)
 
     for c in range(len(board)):
         if board[row][c] == " ":
             tmp_board = board_deepcopy(board)
             tmp_board[row][c] = "Q"
             xout(tmp_board, row, c)
-            solutions = recursive_solve(tmp_board, row + 1,
-                                        queens + 1, solutions)
+            solutions = recursive_solve(tmp_board,
+                                        row + 1, queens + 1, solutions)
 
-    return solutions
+    return (solutions)
 
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
-    if not sys.argv[1].isdigit():
+    if sys.argv[1].isdigit() is False:
         print("N must be a number")
         sys.exit(1)
     if int(sys.argv[1]) < 4:
