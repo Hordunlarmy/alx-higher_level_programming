33a4040ff9816b2c94d6ca853ae63d063daa7c8d
Hordunlarmy <Hordunlarmy@gmail.com>
2023-09-08 20:46:43 +0100

    Fix type check and add type hinting in add_integer function

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -8,8 +8,10 @@ def add_integer(a, b=98):
     """
     a and b must be integers or floats, otherwise raise a TypeError
     """
-    if ((not isinstance(a, int) and not isinstance(a, float))):
+
+    if (type(a) not in (int, float)):
         raise TypeError("a must be an integer")
-    if ((not isinstance(b, int) and not isinstance(b, float))):
+    if (type(b) not in (int, float)):
         raise TypeError("b must be an integer")
+
     return (int(a) + int(b))
