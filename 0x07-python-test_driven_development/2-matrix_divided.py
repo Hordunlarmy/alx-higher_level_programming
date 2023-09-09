c4b132f8df7caf2fa9c1b56fc3fbc12c76f1c494
Hordunlarmy <Hordunlarmy@gmail.com>
2023-09-09 20:23:10 +0100

    Refactor matrix_divided function to handle division by zero

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -12,16 +12,16 @@ def matrix_divided(matrix, div):
     for row in matrix:
         for element in row:
             if type(element) not in (int, float):
-                raise TypeError("matrix must be \
-                        matrix (list of lists) of integers/floats")
+                raise TypeError("matrix must be matrix (list of lists)\
+                        of integers/floats")
 
         if len(row) != len(matrix[0]):
-            raise ValueError("Each row of the matrix must have the same size")
-
-    if (div == 0):
-        raise TypeError("div must be a number")
+            raise TypeError("Each row of the matrix must have the same size")
 
     if type(div) not in (int, float):
+        raise TypeError("div must be a number")
+
+    if (div == 0):
         raise ZeroDivisionError("division by zero")
 
     new_matrix = []
