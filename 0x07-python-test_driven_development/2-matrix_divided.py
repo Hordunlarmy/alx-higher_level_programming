1eceb84bb833c2e93dd0e831f4b54dd5b15a0077
Hordunlarmy <Hordunlarmy@gmail.com>
2023-09-09 20:04:57 +0100

    Add matrix_divided function and test cases for division

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+Function to divide a matrix(a 3 by 2 list)
+"""
+
+
+def matrix_divided(matrix, div):
+    """
+        Checks for conditions of the matrix and div values
+        before performing division logic
+    """
+    for row in matrix:
+        for element in row:
+            if type(element) not in (int, float):
+                raise TypeError("matrix must be \
+                        matrix (list of lists) of integers/floats")
+
+        if len(row) != len(matrix[0]):
+            raise ValueError("Each row of the matrix must have the same size")
+
+    if (div == 0):
+        raise TypeError("div must be a number")
+
+    if type(div) not in (int, float):
+        raise ZeroDivisionError("division by zero")
+
+    new_matrix = []
+    for row in matrix:
+        new_row = []
+        for element in row:
+            result = element / div
+            new_row.append(round(result, 2))
+        new_matrix.append(new_row)
+
+    return (new_matrix)
