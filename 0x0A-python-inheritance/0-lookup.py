8fe297463a1f5598bd6e38bbd752317b0bbac503
Hordunlarmy <Hordunlarmy@gmail.com>
2023-09-11 23:05:50 +0100

    Prints a sorted list in ascending order

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+"""
+A function that returns the list of available attributes
+and methods of an object
+"""
+
+
 def lookup(obj):
-    """
-    A function that returns the list of available attributes
-    and methods of an object
-    """
+    """Prints a sorted list in ascending order"""
 
     return (dir(obj))
