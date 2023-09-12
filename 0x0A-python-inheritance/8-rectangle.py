ccdfe486c405395c49e4377e8ada1848b9656b00
Hordunlarmy <Hordunlarmy@gmail.com>
2023-09-12 02:34:21 +0100

    Add class initialization to Rectangle class

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,25 +1,8 @@
 #!/usr/bin/python3
 """
-A class BaseGeometry (based on 6-base_geometry.py).
+A class Rectangle that inherits from BaseGeometry
 """
-
-
-class BaseGeometry:
-    """Class definition"""
-
-    def area(self):
-        """raise an error"""
-
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """Validates a value"""
-
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
@@ -27,6 +10,8 @@ class Rectangle(BaseGeometry):
         Child of BaseGeometry
     """
     def __init__(self, width, height):
+        """Class initialization"""
+
         super().integer_validator("width", width)
         self.__width = width
         super().integer_validator("height", height)
