dfa88743726ee79539693b99c6f3be445b2272e1
Hordunlarmy <Hordunlarmy@gmail.com>
2023-09-12 02:46:37 +0100

    Refactor Square class to remove unnecessary comments and fix formatting

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,35 +1,15 @@
 #!/usr/bin/python3
 """
-a class Square that inherits from Rectangle (9-rectangle.py)
+a class Square that inherits from Rectangle
 """
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
-
-
-class Rectangle(BaseGeometry):
-    """Class Defined"""
-
-    def __init__(self, width, height):
-        """Class initialized"""
-
-        super().integer_validator("width", width)
-        self.__width = width
-        super().integer_validator("height", height)
-        self.__height = height
-
-    def area(self):
-        return self.__width * self.__height
-
-    def __str__(self):
-        return "[{}] {}/{}".format(self.__class__.__name__,
-                                   self.__width, self.__height)
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """Square class defined."""
+    """Square class defined"""
 
     def __init__(self, size):
-        """Square class initialized."""
-
+        """Square class initialized"""
         self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
