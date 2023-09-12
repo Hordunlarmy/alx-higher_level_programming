86bb6fe4430ef2bff56ed9b9effcaad0be61a0ba
Hordunlarmy <Hordunlarmy@gmail.com>
2023-09-12 01:14:38 +0100

    Fix bug in `is_same_class` function

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -8,7 +8,7 @@ an instance of the specified class
 def is_same_class(obj, the_class):
     "Class Definition"
 
-    if not isinstance(obj, the_class):
+    if isinstance(obj, the_class):
         return True
     else:
         return False
