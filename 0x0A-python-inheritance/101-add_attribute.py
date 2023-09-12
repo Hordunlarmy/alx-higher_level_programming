f0831bf947c5854f8b01eb84bd37e0ec58a422eb
Hordunlarmy <Hordunlarmy@gmail.com>
2023-09-12 03:41:51 +0100

    Add function to add new attribute to an object

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """
-a function that adds a new attribute to an object if it’s possible
+a function that adds a new attribute to an object
 """
 
 
 def add_attribute(obj, att, value):
-
+    """adds new object if possible"""
     if not hasattr(obj, "__dict__"):
         raise TypeError("can't add new attribute")
     setattr(obj, att, value)
