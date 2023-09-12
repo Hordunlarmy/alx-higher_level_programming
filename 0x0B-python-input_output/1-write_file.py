1402c87e063e7901aae96e400caa44843a945b04
Hordunlarmy <Hordunlarmy@gmail.com>
2023-09-12 21:37:40 +0100

    Write file and return number of characters written

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -8,8 +8,8 @@ and returns the number of characters written
 def write_file(filename="", text=""):
     """ function definition"""
 
-    with open(filename, encoding="utf-8", mode="w") as myFile:
+    with open(filename, "w", encoding="utf-8") as myFile:
         myFile.write(text)
 
-    with open(filename, encoding="utf-8") as myFile:
+    with open(filename) as myFile:
         return len(myFile.readline())
