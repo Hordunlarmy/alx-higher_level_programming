1f78fcd67418598b14cb50b0046057a99a47246d
Hordunlarmy <Hordunlarmy@gmail.com>
2023-09-13 19:03:48 +0100

    Add pascal_triangle function to generate Pascal's Triangle

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+"""
+Pascal's Triangle Program
+"""
+
+
+def pascal_triangle(n):
+    """
+    Returns a list of lists of integers representing the Pascal triangle of n.
+    """
+    my_list = []
+    if n <= 0:
+        return my_list
+
+    for j in range(n):
+        row = [1]
+        if my_list:
+            prev_row = my_list[-1]
+            for i in range(1, j):
+                row.append(prev_row[i - 1] + prev_row[i])
+            row.append(1)
+        my_list.append(row)
+
+    return my_list
