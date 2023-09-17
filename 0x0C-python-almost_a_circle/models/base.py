462088cd8d10ad8b932497ad71f1517727dd97a7
Hordunlarmy <Hordunlarmy@gmail.com>
2023-09-17 13:16:35 +0100

    Fix issue with id generation in Base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -15,5 +15,5 @@ class Base:
         if id is not None:
             self.id = id
         else:
-            super().__nb_objects += 1
-            self.id = super().__nb_objects
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
