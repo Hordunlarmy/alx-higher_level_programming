81f4e1d2a6e1984ab6a87b87fb80008f4cb18e1d
Hordunlarmy <Hordunlarmy@gmail.com>
2023-09-17 14:37:50 +0100

    Add display method to Rectangle class

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -81,7 +81,11 @@ class Rectangle(Base):
     def display(self):
         """ prints in stdout the Rectangle instance with the character #"""
 
+        for y in range(self.__y):
+            print("")
         for row in range(self.__height):
+            for x in range(self.__x):
+                print(" ", end="")
             for col in range(self.__width):
                 print("#", end="")
             print()
