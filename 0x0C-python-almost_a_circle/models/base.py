50da00b0855afe9d53ec08902aad7e2402d61b64
Hordunlarmy <Hordunlarmy@gmail.com>
2023-09-18 13:58:45 +0100

    Add Square to_dictionary method and Base to_json_string method

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,7 @@
 """
 Base Class
 """
+import json
 
 
 class Base:
@@ -17,3 +18,9 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """returns the JSON string representation of list_dictionaries"""
+        if list_dictionaries is None or len(list_dictionaries) < 1:
+            return "[]"
+        return json.dumps(list_dictionaries)
--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -55,3 +55,11 @@ class Square(Rectangle):
                 self.x = kwargs['x']
             if 'y' in kwargs:
                 self.y = kwargs['y']
+
+    def to_dictionary(self):
+        """Returns the dictionary representation of a Square"""
+
+        dict_rep = {'id': self.id, 'size': self.size, 'x': self.x,
+                    'y': self.y}
+
+        return (dict_rep)
