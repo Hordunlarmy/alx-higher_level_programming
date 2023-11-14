5c05ac4f648cda412619e05c51d636f002d098f9
hordunlarmy <hordunlarmy@gmail.com>
2023-11-14 20:25:53 +0000

    Add script to compute dictionary of user ids by occurrence

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -2,7 +2,6 @@
 //  a script that imports a dictionary of occurrences by user id and
 //  computes a dictionary of user ids by occurrence.
 
-
 const dict = require('./101-data.js').dict;
 
 const newDict = {};
