8a146a401d9cb5ae6d5d246b61c3b0de043c6827
hordunlarmy <hordunlarmy@gmail.com>
2023-11-14 20:10:52 +0000

    Refactor code to use `map` method for computing a new array

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -2,10 +2,6 @@
 // a script that imports an array and computes a new array
 
 const list = require('./100-data').list;
-const newList = [];
-
-for (let i = 0; i < list.length; i++) {
-  newList.push(list[i] * i);
-}
+const newList = list.map((array, i) => array * i);
 console.log(list);
 console.log(newList);
