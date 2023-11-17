257829c42a9c2f810676da02d2219412198abafc
hordunlarmy <hordunlarmy@gmail.com>
2023-11-17 07:57:46 +0000

    Refactor MySQL connection and cursor usage

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,14 +7,21 @@ import sys
 if __name__ == '__main__':
 
     db = MySQLdb.connect(
-        host="localhost", user=sys.argv[1],  passwd=sys.argv[2],
-        db=sys.argv[3])
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
 
-    db = db.cursor()
+    cursor = db.cursor()
 
-    db.execute("SELECT * FROM states")
+    cursor.execute("SELECT * FROM states")
 
-    rows = db.fetchall()
+    rows = cursor.fetchall()
 
     for row in rows:
         print(row)
+
+    cursor.close()
+    db.close()
