93a73898dc98ec9e3dc26435489f084096311ba1
hordunlarmy <hordunlarmy@gmail.com>
2023-11-19 07:39:55 +0000

    Add model_city and model_city_fetch_by_state to repository

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+a python file that contains the class definition of a City and an instance
+Base = declarative_base()
+"""
+from sqlalchemy import Column, Integer, String, ForeignKey
+from model_state import Base, State
+import sys
+
+
+class City(Base):
+    """
+    City class definition
+    """
+    __tablename__ = 'cities'
+
+    id = Column(Integer, primary_key=True)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+    name = Column(String(128), nullable=False)
