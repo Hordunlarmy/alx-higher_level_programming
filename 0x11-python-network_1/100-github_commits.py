d173b4d78e0f307ca7671c7b039c6e2ce04a4af5
hordunlarmy <hordunlarmy@gmail.com>
2023-12-03 13:09:23 +0000

    Refactor code to remove unnecessary try-except block

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -9,10 +9,7 @@ if __name__ == "__main__":
     r = requests.get(
         f"https://api.github.com/repos/{sys.argv[1]}/{sys.argv[2]}/commits")
 
-    try:
-        for i in range(10):
-            sha = r.json()[i].get("sha")
-            commit = r.json()[i].get("commit").get("author").get("name")
-            print(f"{sha}: {commit}")
-    except IndexError:
-        pass
+    for i in range(10):
+        sha = r.json()[i].get("sha")
+        commit = r.json()[i].get("commit").get("author").get("name")
+        print(f"{sha}: {commit}")
